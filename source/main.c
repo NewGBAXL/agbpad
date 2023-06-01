fab8eedb739451d86575f13fe2833bd59d50f97d
NewGBAXL <81663474+NewGBAXL@users.noreply.github.com>
2023-05-31 21:17:33 -0400

    Edits

---
+++ source/main.c
@@ -0,0 +1,274 @@
+/*
+ * Copyright (C) 2016 FIX94
+ * Copyright (C) 2022 NewGBAXL
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+#include <gccore.h>
+#include <stdio.h>
+#include <malloc.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+//from my tests 50us seems to be the lowest
+//safe si transfer delay in between calls
+#define SI_TRANS_DELAY 50
+
+extern u8 gba_mb_gba[];
+extern u32 gba_mb_gba_size;
+
+static bool AGBPAD_BUTTON_B = false;
+static bool AGBPAD_BUTTON_A = false;
+static bool AGBPAD_BUTTON_SELECT = false;
+static bool AGBPAD_BUTTON_START = false;
+static bool AGBPAD_BUTTON_UP = false;
+static bool AGBPAD_BUTTON_DOWN = false;
+static bool AGBPAD_BUTTON_LEFT = false;
+static bool AGBPAD_BUTTON_RIGHT = false;
+static bool AGBPAD_BUTTON_L = false;
+static bool AGBPAD_BUTTON_R = false;
+
+u8 *resbuf,*cmdbuf;
+volatile u16 pads = 0;
+volatile bool ctrlerr = false;
+void ctrlcb(s32 chan, u32 ret)
+{
+	if(ret)
+	{
+		ctrlerr = true;
+		return;
+	}
+	//just call us again
+	pads = (~((resbuf[1]<<8)|resbuf[0]))&0x3FF;
+	SI_Transfer(1,cmdbuf,1,resbuf,5,ctrlcb,SI_TRANS_DELAY);
+}
+
+volatile u32 transval = 0;
+void transcb(s32 chan, u32 ret)
+{
+	transval = 1;
+}
+
+volatile u32 resval = 0;
+void acb(s32 res, u32 val)
+{
+	resval = val;
+}
+
+unsigned int docrc(u32 crc, u32 val)
+{
+	int i;
+	for(i = 0; i < 0x20; i++)
+	{
+		if((crc^val)&1)
+		{
+			crc>>=1;
+			crc^=0xa1c1;
+		}
+		else
+			crc>>=1;
+		val>>=1;
+	}
+	return crc;
+}
+
+void endproc()
+{
+	printf("GC Button pressed, exit\n");
+	exit(0);
+}
+unsigned int calckey(unsigned int size)
+{
+	unsigned int ret = 0;
+	size=(size-0x200) >> 3;
+	int res1 = (size&0x3F80) << 1;
+	res1 |= (size&0x4000) << 2;
+	res1 |= (size&0x7F);
+	res1 |= 0x380000;
+	int res2 = res1;
+	res1 = res2 >> 0x10;
+	int res3 = res2 >> 8;
+	res3 += res1;
+	res3 += res2;
+	res3 <<= 24;
+	res3 |= res2;
+	res3 |= 0x80808080;
+
+	if((res3&0x200) == 0)
+	{
+		ret |= (((res3)&0xFF)^0x4B)<<24;
+		ret |= (((res3>>8)&0xFF)^0x61)<<16;
+		ret |= (((res3>>16)&0xFF)^0x77)<<8;
+		ret |= (((res3>>24)&0xFF)^0x61);
+	}
+	else
+	{
+		ret |= (((res3)&0xFF)^0x73)<<24;
+		ret |= (((res3>>8)&0xFF)^0x65)<<16;
+		ret |= (((res3>>16)&0xFF)^0x64)<<8;
+		ret |= (((res3>>24)&0xFF)^0x6F);
+	}
+	return ret;
+}
+void doreset()
+{
+	cmdbuf[0] = 0xFF; //reset
+	transval = 0;
+	SI_Transfer(1,cmdbuf,1,resbuf,3,transcb,SI_TRANS_DELAY);
+	while(transval == 0) ;
+}
+void getstatus()
+{
+	cmdbuf[0] = 0; //status
+	transval = 0;
+	SI_Transfer(1,cmdbuf,1,resbuf,3,transcb,SI_TRANS_DELAY);
+	while(transval == 0) ;
+}
+u32 recv()
+{
+	memset(resbuf,0,32);
+	cmdbuf[0]=0x14; //read
+	transval = 0;
+	SI_Transfer(1,cmdbuf,1,resbuf,5,transcb,SI_TRANS_DELAY);
+	while(transval == 0) ;
+	return *(vu32*)resbuf;
+}
+void send(u32 msg)
+{
+	cmdbuf[0]=0x15;cmdbuf[1]=(msg>>0)&0xFF;cmdbuf[2]=(msg>>8)&0xFF;
+	cmdbuf[3]=(msg>>16)&0xFF;cmdbuf[4]=(msg>>24)&0xFF;
+	transval = 0;
+	resbuf[0] = 0;
+	SI_Transfer(1,cmdbuf,5,resbuf,1,transcb,SI_TRANS_DELAY);
+	while(transval == 0) ;
+}
+int main(int argc, char *argv[]) 
+{
+	void *xfb = NULL;
+	GXRModeObj *rmode = NULL;
+	VIDEO_Init();
+	rmode = VIDEO_GetPreferredMode(NULL);
+	xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
+	VIDEO_Configure(rmode);
+	VIDEO_SetNextFramebuffer(xfb);
+	VIDEO_SetBlack(FALSE);
+	VIDEO_Flush();
+	VIDEO_WaitVSync();
+	if(rmode->viTVMode&VI_NON_INTERLACE) VIDEO_WaitVSync();
+	int x = 24, y = 32, w, h;
+	w = rmode->fbWidth - (32);
+	h = rmode->xfbHeight - (48);
+	CON_InitEx(rmode, x, y, w, h);
+	VIDEO_ClearFrameBuffer(rmode, xfb, COLOR_BLACK);
+	PAD_Init();
+	cmdbuf = memalign(32,32);
+	resbuf = memalign(32,32);
+	int i;
+	while(1)
+	{
+		printf("\x1b[2J");
+		printf("\x1b[37m");
+		printf("Simple GBA Input Viewer by FIX94\n");
+		printf("You can press any GC controller button to quit\n");
+		printf("Waiting for GBA in port 2...\n");
+		resval = 0;
+		ctrlerr = false;
+
+		SI_GetTypeAsync(1,acb);
+		while(1)
+		{
+			if(resval)
+			{
+				if(resval == 0x80 || resval & 8)
+				{
+					resval = 0;
+					SI_GetTypeAsync(1,acb);
+				}
+				else if(resval)
+					break;
+			}
+			PAD_ScanPads();
+			VIDEO_WaitVSync();
+			if(PAD_ButtonsHeld(0))
+				endproc();
+		}
+		if(resval & SI_GBA)
+		{
+			//set gbaConnected to be true
+			printf("GBA Found!\n");
+			resbuf[2]=0;
+			while(!(resbuf[2]&0x10))
+			{
+				doreset();
+				getstatus();
+			}
+			printf("Ready, sending input stub\n");
+			unsigned int sendsize = ((gba_mb_gba_size+7)&~7);
+			unsigned int ourkey = calckey(sendsize);
+			printf("Our Key: %08x\n", ourkey);
+			//get current sessionkey
+			u32 sessionkeyraw = recv();
+			u32 sessionkey = __builtin_bswap32(sessionkeyraw^0x7365646F);
+			//send over our own key
+			send(__builtin_bswap32(ourkey));
+			unsigned int fcrc = 0x15a0;
+			//send over gba header
+			for(i = 0; i < 0xC0; i+=4)
+			{
+				send(__builtin_bswap32(*(vu32*)(gba_mb_gba+i)));
+				if(!(resbuf[0]&0x2)) printf("Possible error %02x\n",resbuf[0]);
+			}
+			printf("Header done! Sending ROM...\n");
+			for(i = 0xC0; i < sendsize; i+=4)
+			{
+				u32 enc = ((gba_mb_gba[i+3]<<24)|(gba_mb_gba[i+2]<<16)|(gba_mb_gba[i+1]<<8)|(gba_mb_gba[i]));
+				fcrc=docrc(fcrc,enc);
+				sessionkey = (sessionkey*0x6177614B)+1;
+				enc^=sessionkey;
+				enc^=((~(i+(0x20<<20)))+1);
+				enc^=0x20796220;
+				send(enc);
+				if(!(resbuf[0]&0x2)) printf("Possible error %02x\n",resbuf[0]);
+			}
+			fcrc |= (sendsize<<16);
+			printf("ROM done! CRC: %08x\n", fcrc);
+			//send over CRC
+			sessionkey = (sessionkey*0x6177614B)+1;
+			fcrc^=sessionkey;
+			fcrc^=((~(i+(0x20<<20)))+1);
+			fcrc^=0x20796220;
+			send(fcrc);
+			//get crc back (unused)
+			recv();
+			//start read chain
+			cmdbuf[0] = 0x14; //read
+			transval = 0;
+			//delay needed for gba fadeout
+			SI_Transfer(1,cmdbuf,1,resbuf,5,ctrlcb,1000000);
+			//hm
+			while(1)
+			{
+				if(ctrlerr) break;
+				
+				//NewGBAXL edit
+				AGB_BUTTON_A = (!!(pads & 1));
+				AGB_BUTTON_B = (!!(pads & 2));
+				AGB_BUTTON_SELECT = (!!(pads & 4));
+				AGB_BUTTON_START = (!!(pads & 8));
+				AGB_BUTTON_RIGHT = (!!(pads & 16));
+				AGB_BUTTON_LEFT = (!!(pads & 32));
+				AGB_BUTTON_UP = (!!(pads & 64));
+				AGB_BUTTON_DOWN = (!!(pads & 128));
+				AGB_BUTTON_R = (!!(pads & 256));
+				AGB_BUTTON_L = (!!(pads & 512));
+
+				printf("\rA:%i B:%i Select:%i Start:%i Right:%i Left:%i Up:%i Down:%i R:%i L:%i",
+					!!(pads&1),!!(pads&2),!!(pads&4),!!(pads&8),!!(pads&16),!!(pads&32),!!(pads&64),!!(pads&128),!!(pads&256),!!(pads&512));
+				VIDEO_WaitVSync();
+			}
+		}
+	}
+	return 0;
+}
